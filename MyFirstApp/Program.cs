8c6efe9bb9ed6098988f14e1875a983955231887
IlijaP81 <niilp1999no@gmail.com>
2024-07-22 14:16:58 +0300

    Добавьте файлы проекта.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Привет, \n мир");
+        Console.WriteLine("Symbol #: \u0023 (u + 0023)");
+    }
+}
